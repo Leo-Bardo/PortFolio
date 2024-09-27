cff496a361263f3af1db1097703c936072b5a66f
unknown <julioalb4488@gmail.com>
2024-09-27 07:08:53 -0600

    encabezado de portfolio

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+			<div class="logo">
+					<a href="#">Julio Muñoz</a>
+			</div>
+				<nav id="nav" class="contenedor-header">
+					<ul id="menu">
+						<li><a href="#inicio">INICIO</a> </li>
+						<li><a href="#sobremi">SOBRE MI</a> </li>
+						<li><a href="#skills">SKILLS</a> </li>
+						<li><a href="#curriculum">CURRICULUM</a> </li>
+						<li><a href="#portfolio">PORTFOLIO</a> </li>
+						<li><a href="#contacto">CONTACTO</a> </li>
+					</ul>
+				</nav>
+				{/* <!-- Programar botón que destruya todos los frames de mi página y lance una alerta --> */}
+				<div class="nav-responsive">
+					<i class="fa-solid fa-bars"></i>
+				</div> 
       </header>
     </div>
   );
